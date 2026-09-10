787b0b9063e2ae048cf65f04c26695a0c6a0168a
tala <tala.aunv@gmail.com>
2026-09-10 19:50:01 +0300

    update app id

--- professional_security_app/android/app/build.gradle.kts
+++ professional_security_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.halabi.professional_security"
+    namespace = "com.halabi.professionalsecurity"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.halabi.professional_security"
+        applicationId = "com.halabi.professionalsecurity"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- professional_security_app/android/app/src/main/kotlin/com/halabi/professionalsecurity/MainActivity.kt
+++ professional_security_app/android/app/src/main/kotlin/com/halabi/professionalsecurity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.halabi.professional_security
+package com.halabi.professionalsecurity
 
 import io.flutter.embedding.android.FlutterActivity
 
